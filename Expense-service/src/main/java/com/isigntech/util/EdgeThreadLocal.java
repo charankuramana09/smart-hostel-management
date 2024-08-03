149fcb39f5d571624d729733507826f85a95e27b
charan <160104625+charankuramana09@users.noreply.github.com>
2024-08-03 16:27:36 +0530

    Delete Expense-service directory

--- Expense-service/src/main/java/com/isigntech/util/EdgeThreadLocal.java
+++
@@ -1,9 +0,0 @@
-package com.isigntech.util;
-
-
-import java.util.HashMap;
-import java.util.Map;
-
-public class EdgeThreadLocal {
-	public static ThreadLocal<Map<String, String>> edgeThreadLocalholder = ThreadLocal.withInitial(HashMap::new);
-}
